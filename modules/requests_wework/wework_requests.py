de6d39bd5553609261387b8755d0f089572c860d
aliya <329274931@qq.com>
2024-07-05 13:13:40 +0800

    added test case

--- modules/requests_wework/wework_requests.py
+++ modules/requests_wework/wework_requests.py
@@ -10,18 +10,18 @@ from requesturl.wework_request_url import RequestUrl
 
 class WeworkModules(object):
 
-    # @staticmethod
-    # def get_wework_token_case():
-    #     """
-    #     发送请求获取企业微信 access_token
-    #     corp_secret 为 获取通讯录的 secret
-    #     """
-    #     corp_info = read_yaml(file_path="testdata\\wework_data\\data_before_login.yml")
-    #     corp_id = corp_info["corp_id"]
-    #     corp_secret = corp_info["corp_secret"]
-    #     payload = {"corpid": corp_id, "corpsecret": corp_secret}
-    #     response = requests.get(url=RequestUrl.get_wework_token_uri(), params=payload)
-    #     return response
+    @staticmethod
+    def get_wework_token_case():
+        """
+        发送请求获取企业微信 access_token
+        corp_secret 为 获取通讯录的 secret
+        """
+        corp_info = read_yaml(file_path="testdata\\wework_data\\data_before_login.yml")
+        corp_id = corp_info["corp_id"]
+        corp_secret = corp_info["corp_secret"]
+        payload = {"corpid": corp_id, "corpsecret": corp_secret}
+        response = requests.get(url=RequestUrl.get_wework_token_uri(), params=payload)
+        return response
 
     @staticmethod
     def contact_list_create_member(get_wework_token):
@@ -30,8 +30,8 @@ class WeworkModules(object):
         如果报错 IP 不在可信 IP 范围，加上可信 IP：
         https://work.weixin.qq.com/wework_admin/frame#apps/contactsApi
         """
-        # access_token = get_wework_token.json().get("access_token")
-        # param = {"access_token": access_token}
+        access_token = get_wework_token.json().get("access_token")
+        param = {"access_token": access_token}
         member_info = read_yaml(file_path="testdata\\wework_data\\contact_list.yml")
         # data 数据放在 yaml 文件中，使用如下方法可以通过 key 获取 value，继续.get('skills', []).get(0, 'Not Found')读取第一个 skill
         mem_info_user_id = Public.random_str()
@@ -47,7 +47,7 @@ class WeworkModules(object):
         # 将修改后的数据写回 yaml 文件
         with open("contact_list.yml", 'w') as file:
             yaml.dump(mem_info, file, default_flow_style=False)
-        response = requests.post(url=RequestUrl.contact_list_create_member(), json=mem_info)
+        response = requests.post(url=RequestUrl.contact_list_create_member(), params=param, json=mem_info)
         # print('SAAA', response.json())
         return response
 
--- testcase/test_wework.py
+++ testcase/test_wework.py
@@ -3,10 +3,10 @@ from modules.requests_wework.wework_requests import WeworkModules
 
 class TestWeworkRequests(object):
 
-    # def test_wework_get_token_case(self):
-    #     response = WeworkModules.get_wework_token_case()
-    #     assert response.json().get("errcode") == 0, "未获取到企业微信 access token"
-    #     assert response.json().get("errmsg") == "ok"
+    def test_wework_get_token_case(self):
+        response = WeworkModules.get_wework_token_case()
+        assert response.json().get("errcode") == 0, "未获取到企业微信 access token"
+        assert response.json().get("errmsg") == "ok"
 
     def test_contact_list_create_member(self, get_wework_token):
         response = WeworkModules.contact_list_create_member(get_wework_token)
